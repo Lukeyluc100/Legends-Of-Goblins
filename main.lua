ff9579eff9ea3ab4c4e1649f65b208a5b27931c1
Lukeyluc100 <154043763+Lukeyluc100@users.noreply.github.com>
2025-08-20 20:39:14 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,30 +3,30 @@ function love.load()
 
     character = {}
     character.x = 800
-    character.y = -400
-    character.speed = 200 -- Pixel pro Sekunde
+    character.y = 150
+    character.speed = 200 
     character.sprite = love.graphics.newImage('assets/GoblinGirl.png')
 
-    -- Zielkoordinaten
-    targetX = 400
-    targetY = 200
+    
+    targetX = 550
+    targetY = 150
 end
 
 function love.update(dt)
-    -- Unterschied zwischen Ziel und aktueller Position
+    
     local dx = targetX - character.x
     local dy = targetY - character.y
 
-    -- Abstand berechnen
+    
     local distance = math.sqrt(dx*dx + dy*dy)
 
-    -- Nur bewegen, wenn noch nicht am Ziel
+    
     if distance > 1 then
-        -- Normalisierte Richtung
+        
         local dirX = dx / distance
         local dirY = dy / distance
 
-        -- Bewegung
+        
         character.x = character.x + dirX * character.speed * dt
         character.y = character.y + dirY * character.speed * dt
     end
