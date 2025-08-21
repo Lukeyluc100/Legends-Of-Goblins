902e4a33a55a8a512d1b20b450d09e13568b31cd
Lukeyluc100 <154043763+Lukeyluc100@users.noreply.github.com>
2025-08-21 07:15:22 +0200

    Intro Seqeunze and Textbox added

--- main.lua
+++ main.lua
@@ -1,37 +1,49 @@
 function love.load()
     love.graphics.setDefaultFilter("nearest", "nearest")
 
-    character = {}
-    character.x = 800
-    character.y = 150
-    character.speed = 200 
-    character.sprite = love.graphics.newImage('assets/GoblinGirl.png')
+    Enemy = {}
 
-    
-    targetX = 550
-    targetY = 150
+    Enemy.y = -150
+    Enemy.x = 800
+
+    Player = {}
+
+    Player.y = 600
+    Player.x = -100
+
+
+
 end
 
+function lerp(a, b, t)
+    return a + (b - a) * t
+end
+
+
+
 function love.update(dt)
-    
-    local dx = targetX - character.x
-    local dy = targetY - character.y
 
-    
-    local distance = math.sqrt(dx*dx + dy*dy)
 
-    
-    if distance > 1 then
-        
-        local dirX = dx / distance
-        local dirY = dy / distance
+    local EnemyLocalX = 600
+    local EnemyLocalY = 100
 
-        
-        character.x = character.x + dirX * character.speed * dt
-        character.y = character.y + dirY * character.speed * dt
-    end
+    Enemy.x = lerp(Enemy.x, EnemyLocalX, 2 * dt)
+    Enemy.y = lerp(Enemy.y, EnemyLocalY, 2 * dt)
+
+    local PlayerLocalX = 50
+    local PlayerLocalY = 400
+
+    Player.x = lerp(Player.x, PlayerLocalX, 2 * dt)
+    Player.y = lerp(Player.y, PlayerLocalY, 2 * dt)
+
 end
 
 function love.draw()
-    love.graphics.draw(character.sprite, character.x, character.y, 0, 4, 4)
+    love.graphics.setColor(100, 0, 0)
+
+    love.graphics.rectangle('fill', Enemy.x, Enemy.y, 100, 100)
+
+    love.graphics.setColor(0, 0, 100)
+
+    love.graphics.rectangle('fill', Player.x, Player.y, 100, 100)
 end 
