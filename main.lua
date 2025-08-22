ccdbc2e19fc1b0b22040742ed2f99bf13e666a4c
Lukeyluc100 <154043763+Lukeyluc100@users.noreply.github.com>
2025-08-22 07:29:55 +0200

    TextBox Large

--- main.lua
+++ main.lua
@@ -1,41 +1,51 @@
 
 
 function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
 
+    -- Load Sprites
+    love.graphics.setDefaultFilter("nearest", "nearest")
     PixelFont = love.graphics.newFont('assets/fonts/PixelFont.ttf', 24)
     AttackButtonSprite = love.graphics.newImage('assets/AttackButton.png')
     DefendButtonSprite = love.graphics.newImage('assets/DefendButton.png')
+    LargeTextBox = love.graphics.newImage('assets/LargeTextBox.png')
 
-    print(PixelFont:hasGlyphs("0123456789"))
+
+
+
+
+
 
     
     state = "Player Attack"
 
     Enemy = {}
 
-    Enemy.y = -150
-    Enemy.x = 800
+    Enemy.y = 300
+    Enemy.x = 900
     Enemy.Health = 100
     Enemy.Alive = true
     Enemy.AttackDamage = love.math.random(10, 16)
 
     Player = {}
 
-    Player.y = 600
-    Player.x = -100
+    Player.y = 300
+    Player.x = -300
     Player.Health = 100
     Player.Defence = 0
     Player.Alive = true
-    Player.AttackDamage = love.math.random(10, 16)
+    Player.AttackDamage = love.math.random(10, 13, 19)
 
 
 
-    DefendbuttonX = 400
-    DefendbuttonY = 450
-    DefendbuttonWidth = 46 * 2  -- 92
-    DefendbuttonHeight = 16 * 2 -- 32
+    DefendbuttonX = 610
+    DefendbuttonY = 500
+    DefendbuttonWidth = 46 * 3.25
+    DefendbuttonHeight = 16 * 3.25
 
+    AttackbuttonX = 610
+    AttackbuttonY = 410
+    AttackbuttonWidth = 46 * 3.25  
+    AttackbuttonHeight = 16 * 3.25
 
 end
 
@@ -49,13 +59,13 @@ function love.update(dt)
 
 
     local EnemyLocalX = 600
-    local EnemyLocalY = 100
+    local EnemyLocalY = 300
 
     Enemy.x = lerp(Enemy.x, EnemyLocalX, 2 * dt)
     Enemy.y = lerp(Enemy.y, EnemyLocalY, 2 * dt)
 
-    local PlayerLocalX = 50
-    local PlayerLocalY = 400
+    local PlayerLocalX = 100
+    local PlayerLocalY = 300
 
     Player.x = lerp(Player.x, PlayerLocalX, 2 * dt)
     Player.y = lerp(Player.y, PlayerLocalY, 2 * dt)
@@ -72,10 +82,11 @@ function love.update(dt)
 
         state = "BattleWon"
         Enemy.Health = 100
-        Player.y = 600
-        Player.x = -100
-        Enemy.y = -150
-        Enemy.x = 800
+        Player.Health = 100
+        Player.y = 300
+        Player.x = -800
+        Enemy.y = 300
+        Enemy.x = 900
         Enemy.Alive = true
     end
 
@@ -84,10 +95,10 @@ function love.update(dt)
         Player.Alive = true
         Enemy.Health = 100
         Player.Health = 100
-        Player.y = 600
-        Player.x = -100
-        Enemy.y = -150
-        Enemy.x = 800
+        Player.y = 300
+        Player.x = -900
+        Enemy.y = 300
+        Enemy.x = 900
         Enemy.Alive = true   
 
     end
@@ -96,9 +107,12 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(100, 0, 0)
     love.graphics.setFont(PixelFont)
 
+    --Draw UI
+    love.graphics.draw(LargeTextBox, 0, 400, 0, 6.25, 3)
+    -------------------------------------
+    love.graphics.setColor(100, 0, 0)
 
     if Enemy.Alive then    
         love.graphics.rectangle('fill', Enemy.x, Enemy.y, 100, 100)
@@ -113,11 +127,11 @@ function love.draw()
     love.graphics.setColor(100, 100, 100)
 
     if Player.Alive then    
-    love.graphics.print(Player.Health, 80, 350, 0, 1, 1)
+    love.graphics.print("HP " ..Player.Health, 130, 150, 0, 1, 1)
     end
 
     if Enemy.Alive then
-        love.graphics.print(Enemy.Health, 630, 50, 0, 1, 1)
+        love.graphics.print("HP " ..Enemy.Health, 630, 150, 0, 1, 1)
     end
 
     if state == "BattleWon" then
@@ -128,14 +142,15 @@ function love.draw()
         love.graphics.print("You Lose", 350, 300)
     end
 
-    love.graphics.draw(AttackButtonSprite, 400, 400, 0, 2, 2)
-    love.graphics.draw(DefendButtonSprite, 400, 450, 0, 2, 2)
+    love.graphics.draw(AttackButtonSprite, 610, 430, 0, 3.25, 3.25)
+    love.graphics.draw(DefendButtonSprite, 610, 500, 0, 3.25, 3.25)
 end
 
 
 function love.mousepressed(x, y, button)
     if button == 1 then
-        if x > 400 and x < 600 and y > 400 and y < 420 then
+        if x >= AttackbuttonX and x <= AttackbuttonX + AttackbuttonWidth
+        and y >= AttackbuttonY and y <= AttackbuttonY + AttackbuttonHeight then
             if Player.Alive then
                 Enemy.Health = Enemy.Health -Player.AttackDamage
                 state = "Enemy Attack"
